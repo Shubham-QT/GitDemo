b6b1b1d6745290987976851696335ab4b8a4af12
Shubham-QT <shubhamshukla54@gmail.com>
2025-06-30 16:32:03 +0530

    Updated Test3

---
+++ src/Test3.java
@@ -0,0 +1,12 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		String hungry="No";
+		if(hungry=="No"||hungry=="N"||hungry=="n") {
+			System.out.println("Do your work....");
+		}
+		else
+			System.out.println("Eat something");
+	}
+}
